2a4a61de6f963a95f0d3d873e1b30075e1c33a72
Ekliptor <daniel@blockfint.com>
2023-01-23 08:44:48 +0700

    initial commit

---
+++ src/temp/updateProcess.js
@@ -0,0 +1,22 @@
+// this file is the entry point of fork()
+
+const path = require('path')
+const Installer = require(path.join(__dirname, '..', 'Installer'))
+
+const resumeUpdate = (installerData, restartCmd) => {
+    let installer = new Installer(installerData, restartCmd)
+    installer.resumeUpdate().then(() => {
+        // won't be reached
+    }).catch((err) => {
+        Installer.getLogger().error(err)
+        process.exit(1)
+    })
+}
+
+process.on('message', (message) => {
+    setTimeout(() => {
+        resumeUpdate(message.installerData.options, message.restartCmd)
+    }, 600)
+})
+
+setTimeout(() => {}, 2000); // ensure this process keeps running until we get the message from parent
